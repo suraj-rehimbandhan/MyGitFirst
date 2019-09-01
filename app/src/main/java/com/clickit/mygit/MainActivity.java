1ddb578261ef62c54591ac0f9cb67a3e6aa2e6b4
rb-2 <developer.reshimbandhan@gmail.com>
2019-09-01 11:17:49 +0530

    This is fifth commit

--- app/src/main/java/com/clickit/mygit/MainActivity.java
+++ app/src/main/java/com/clickit/mygit/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Success1", Toast.LENGTH_SHORT).show();
 
     }
 }
